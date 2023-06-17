dee68cc17843afb2586044e4d81393669f3e1b67
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-06-17 07:28:13 +0700

    ignore file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 
-def print_directory_tree(path, prefix='', last_path = '', ignore = ['.git']):
+def print_directory_tree(path, prefix='', last_path = '', ignore = ['.git', 'target']):
     if os.path.basename(path) in ignore:
         return
     if os.path.isfile(path):
@@ -12,8 +12,11 @@ def print_directory_tree(path, prefix='', last_path = '', ignore = ['.git']):
             sub_prefix = '   '
         else:
             sub_prefix =  '|   '
-        for filename in sorted(os.listdir(path)):
-            print_directory_tree(os.path.join(path, filename), prefix + sub_prefix, sorted(os.listdir(path))[-1], ignore)
+        list_files = sorted(os.listdir(path))
+        # remove file in ignore
+        list_files = [file for file in list_files if file not in ignore]
+        for filename in list_files:
+            print_directory_tree(os.path.join(path, filename), prefix + sub_prefix, list_files[-1], ignore)
 
 # Ví dụ sử dụng:
 path = "F:\CTDA\spring-mvc-starter-master\spring-mvc-starter-master"
