133430327d30e9594fef98b84d85fe3bd549d491
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2023-06-17 07:11:43 +0700

    config prefix and add ignore

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import os
 
-def print_directory_tree(path, prefix=''):
+ignore = ['.git']
+def print_directory_tree(path, prefix='', last_path = ''):
+    if os.path.basename(path) in ignore:
+        return
     if os.path.isfile(path):
         print(prefix + '|--', os.path.basename(path))
     elif os.path.isdir(path):
         print(prefix + '|--', os.path.basename(path) + '/')
-        prefix += '|   '
+        if os.path.basename(path) == last_path:
+            sub_prefix = '   '
+        else:
+            sub_prefix =  '|   '
         for filename in sorted(os.listdir(path)):
-            print_directory_tree(os.path.join(path, filename), prefix)
+            print_directory_tree(os.path.join(path, filename), prefix + sub_prefix, sorted(os.listdir(path))[-1])
 
 # Ví dụ sử dụng:
-path = "D:\HaiZuka\source"
+path = "F:\CTDA\spring-mvc-starter-master\spring-mvc-starter-master"
 path.replace("\\", "\\\\")
 
-print_directory_tree(path)
+print_directory_tree(path, last_path=path.split("\\")[-1])
